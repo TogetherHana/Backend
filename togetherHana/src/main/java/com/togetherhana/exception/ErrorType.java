976a368e01198bdf24c24de4cb26425b22e373c2
jiyeon <sjy6736@naver.com>
2024-06-20 14:37:27 +0900

    feat: ErrorType 정의

---
+++ togetherHana/src/main/java/com/togetherhana/exception/ErrorType.java
@@ -0,0 +1,39 @@
+package com.togetherhana.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorType {
+
+	/**
+	 * 400 BAD REQUEST
+	 */
+	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+
+	/**
+	 * 401 UNAUTHROZIED
+	 */
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않았습니다."),
+
+	/**
+	 * 404 NOT FOUND
+	 */
+	NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리소스입니다."),
+
+	/**
+	 * 500 INTERNAL SERVER
+	 */
+	INTERNAL_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다");
+
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	public int getHttpStatusCode() {
+		return httpStatus.value();
+	}
+}
