f544bd674288e27d3d418bf75ebd7ae2859618db
chansoo <chansoo38@gmail.com>
2024-06-20 10:37:29 +0900

    init : 프로젝트 생성

---
+++ togetherHana/src/main/java/com/togetherhana/TogetherHanaApplication.java
@@ -0,0 +1,13 @@
+package com.togetherhana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherHanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherHanaApplication.class, args);
+    }
+
+}
---
+++ togetherHana/src/test/java/com/togetherhana/TogetherHanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togetherhana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherHanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
