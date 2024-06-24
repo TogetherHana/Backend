d9f1631ec6a24dd6a1d66e1ca90e70f210b9136c
jiyeon <sjy6736@naver.com>
2024-06-24 20:32:23 +0900

    chore: 안쓰는 의존성 삭제

--- togetherHana/src/main/java/com/togetherhana/mileage/service/MileageService.java
+++ togetherHana/src/main/java/com/togetherhana/mileage/service/MileageService.java
@@ -1,11 +1,8 @@
 package com.togetherhana.mileage.service;
 
-import com.togetherhana.member.repository.MemberRepository;
 import com.togetherhana.mileage.repository.MileageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
