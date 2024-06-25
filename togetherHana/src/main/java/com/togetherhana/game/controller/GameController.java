a1c8afdf67dd27056f86f0b06febb3a92d9a222f
jiyeon <sjy6736@naver.com>
2024-06-25 16:03:25 +0900

    feat: 매치 승자처리 기능 구현

--- togetherHana/src/main/java/com/togetherhana/game/controller/GameController.java
+++ togetherHana/src/main/java/com/togetherhana/game/controller/GameController.java
@@ -1,5 +1,7 @@
 package com.togetherhana.game.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.togetherhana.base.BaseResponse;
 import com.togetherhana.game.dto.request.GameCreateRequestDto;
 import com.togetherhana.game.dto.request.OptionChoiceRequestDto;
+import com.togetherhana.game.dto.response.GameParticipantDto;
+import com.togetherhana.game.dto.response.GameSelectResponseDto;
 import com.togetherhana.game.service.GameService;
 
 import lombok.RequiredArgsConstructor;
@@ -34,9 +38,20 @@ public class GameController {
 		/**
 		 * 추후 수정
 		 */
-		Long memberId = 1L;
-		gameService.vote(memberId, optionChoiceRequestDto);
+		Long memberIdx = 1L;
+		gameService.vote(memberIdx, optionChoiceRequestDto);
 		return BaseResponse.success();
+	}
+
+	@PostMapping("/game/select")
+	public BaseResponse decideGameWinner(@RequestBody OptionChoiceRequestDto optionChoiceRequestDto) {
+
+		/**
+		 * 추후 수정
+		 */
+		Long memberIdx = 2L;
+		GameSelectResponseDto gameSelectResponseDto = gameService.decideGameWinner(memberIdx, optionChoiceRequestDto);
+		return BaseResponse.success(gameSelectResponseDto);
 	}
 
 }
