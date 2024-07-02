62380b831f11a3fe644c944283c77d972b8a2984
lee010207 <jy0260@gmail.com>
2024-07-02 17:39:04 +0900

    refactor : 코드수정에 따른 Service 삭제 및 Controller 수정

--- togetherHana/src/main/java/com/togetherhana/systemEvent/controller/SystemEventController.java
+++ togetherHana/src/main/java/com/togetherhana/systemEvent/controller/SystemEventController.java
@@ -3,7 +3,6 @@ package com.togetherhana.systemEvent.controller;
 import com.togetherhana.auth.jwt.Auth;
 import com.togetherhana.base.BaseResponse;
 import com.togetherhana.member.entity.Member;
-import com.togetherhana.systemEvent.service.SystemEventRedissonService;
 import com.togetherhana.systemEvent.service.SystemEventSortedSetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,23 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/event")
 public class SystemEventController {
 
-    private final SystemEventRedissonService systemEventService;
     private final SystemEventSortedSetService systemEventSortedSetService;
 
     // 선착순 이벤트
-    @GetMapping("/get-ticket/{memberIdx}")
+    @GetMapping("/get-ticket")
     public BaseResponse<Boolean> tryGetTicket(@Auth Member member) {
         systemEventSortedSetService.addOnRedis(member.getMemberIdx());
-        //log.info("Received request for memberIdx: " + memberIdx);
-        //boolean result = systemEventService.tryGetTicket(memberIdx);
-        //log.info("Result for memberIdx: " + memberIdx + " is " + result);
-        //return BaseResponse.success(result);
         return BaseResponse.success(systemEventSortedSetService.getWinner(member.getMemberIdx()));
-        //return BaseResponse.success(systemEventSortedSetService.tryGetTicket(member.getMemberIdx()));
     }
 
-    @PostMapping("/reset")
-    public void resetEvent() {
-        systemEventService.clearEvent();
-    }
 }
